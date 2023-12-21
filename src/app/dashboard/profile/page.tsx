d1c8bfb9a40d218c1b762c158f6362e77dc44546
panchalialo <panchalijob@gmail.com>
2023-12-22 00:15:32 +0530

    chngs

--- src/app/dashboard/profile/page.tsx
+++ src/app/dashboard/profile/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const ProfilePage = () => {
   return (
     <div>
-        <h1 className='text-4xl font-bold'>Profiles Pages</h1>
+        <h1 className='text-4xl font-bold hello'>Profiles Pages</h1>
     </div>
   )
 }
